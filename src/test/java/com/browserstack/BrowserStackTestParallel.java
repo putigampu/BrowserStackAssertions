e999ea615e0ae04ea9bd9f3df2b4aae787e2d210
sree devi <sputigampu@metricstream.com>
2020-03-16 14:50:35 -0700

    Update BrowserStackTestParallel.java

--- src/test/java/com/browserstack/BrowserStackTestParallel.java
+++ src/test/java/com/browserstack/BrowserStackTestParallel.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit
 
 
 public class BrowserStackTestParallel extends BrowserStackTestNGTest {
@@ -11,6 +12,7 @@ public class BrowserStackTestParallel extends BrowserStackTestNGTest {
     @Test
     public void AssertionsTest() throws Exception {
         driver.get("https://www.macys.com/");
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
         WebElement element = driver.findElement(By.id("logo"));
         Assert.assertTrue(element.isDisplayed());
 
